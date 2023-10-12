92c09ef36ddeb504a190d8d2e08fe54c49b8f110
vahsimonian <simonianvah@gmail.com>
2023-10-12 20:17:28 +0700

    cmoomit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Home;
