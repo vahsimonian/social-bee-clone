16f679158fe2f153f388b71a5053d801b3f8ed9b
vahsimonian <simonianvah@gmail.com>
2023-10-12 11:03:15 +0700

    commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const navbarData = [
+  {
+    id: 1,
+    link: "about",
+  },
+  {
+    id: 2,
+    link: "portfolio",
+  },
+  {
+    id: 3,
+    link: "experience",
+  },
+  {
+    id: 4,
+    link: "contact",
+  },
+  {
+    id: 5,
+    link: "contact",
+  },
+  {
+    id: 6,
+    link: "contact",
+  },
+  {
+    id: 7,
+    link: "contact",
+  },
+];
