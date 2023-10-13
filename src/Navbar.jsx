bee18a9c7267e61dbeffdd90b81ed79585a218a3
vahsimonian <simonianvah@gmail.com>
2023-10-13 13:33:46 +0700

    commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,11 +6,13 @@ import socialBeeIcon from "../src/assets/socialbee-logo.png";
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const element = navbarData.map((item) => <a>{item.link}</a>);
+  const element = navbarData.map((item) => (
+    <a className="nav-links">{item.link}</a>
+  ));
   return (
     <nav className="navbar">
       <img src={socialBeeIcon} alt="socialBee" className="nav-img" />
-      <div className="nav-links">{element}</div>
+      {element}
       <button className="sign-btn">Sing Up / Login</button>
     </nav>
   );
