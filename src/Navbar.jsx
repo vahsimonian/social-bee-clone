c1a0755dacc22d5d4b236cdb68993052386258bb
vahsimonian <simonianvah@gmail.com>
2023-10-15 13:00:51 +0700

    commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
 
   const element = navbarData.map((item) => (
     <div className="nav-icon-wrap">
-      <li className="nav-link curs-point">
+      <li className="nav-link curs-point nav-hover">
         {item.link}
         <img src={arrowDown} width={35} alt="" className="arrow-down-bold" />
       </li>
@@ -22,17 +22,18 @@ function Navbar() {
         <img src={socialBeeIcon} alt="socialBee" className="nav-img" />
       </div>
       <div className="iva">
-        <span className="curs-point">Home</span>
+        <span className="curs-point home-icon nav-hover">
+          <img src={homeIcon} alt="home" width={15} />
+          Home
+        </span>
         {element}
-        <span className="curs-point">Book a Call</span>
-        <span className="curs-point">Overview</span>
+        <span className="curs-point nav-hover">Book a Call</span>
+        <span className="curs-point nav-hover">Overview</span>
       </div>
-      <div>
-        <button className="sign-btn">
-          <span className="curs-point">Sing Up / </span>
-          <span className="curs-point">Login</span>
-        </button>
-      </div>
+      <button className="sign-btn">
+        <span className="curs-point">Sing Up / </span>
+        <span className="curs-point">Login</span>
+      </button>
     </nav>
   );
 }
