5a29f75cb7c625a56cacb6175fd5bfade6706bff
vahsimonian <simonianvah@gmail.com>
2023-10-16 11:34:09 +0700

    commit

--- src/data.jsx
+++ src/data.jsx
@@ -1,4 +1,15 @@
 import arrowDown from "./assets/down-arrow.png";
+import facebookIcon from "./assets/facebook-line.png";
+import instagramIcon from "./assets/instagram-line.png";
+import youtubeIcon from "./assets/youtube-line.png";
+import adIcon from "./assets/advertisement-line.png";
+import editBoxLine from "./assets/edit-box-line.png";
+import heartIcon from "./assets/heart-line.png";
+import homeFillIcon from "./assets/home-4-fill.png";
+import linkedinIcon from "./assets/linkedin-box-line.png";
+import pinterestIcon from "./assets/pinterest-line.png";
+import storeIcon from "./assets/store-2-line.png";
+import twitterIcon from "./assets/twitter-x-line.png";
 
 export const navbarData = [
   {
@@ -20,5 +31,41 @@ export const navbarData = [
     id: 4,
     link: "Resources",
     // arrowDown: arrowDown,
+  },
+];
+
+const iconsData = [
+  {
+    instagramIcon: instagramIcon,
+  },
+  {
+    facebookIcon: facebookIcon,
+  },
+  {
+    youtubeIcon: youtubeIcon,
+  },
+  {
+    adIcon: adIcon,
+  },
+  {
+    editBoxLine: editBoxLine,
+  },
+  {
+    heartIcon: heartIcon,
+  },
+  {
+    homeFillIcon: homeFillIcon,
+  },
+  {
+    linkedinIcon: linkedinIcon,
+  },
+  {
+    pinterestIcon: pinterestIcon,
+  },
+  {
+    storeIcon: storeIcon,
+  },
+  {
+    twitterIcon: twitterIcon,
   },
 ];
