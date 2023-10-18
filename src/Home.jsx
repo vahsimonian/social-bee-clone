af093eb6032cf7b16125161cdd2681ec1350b993
vahsimonian <simonianvah@gmail.com>
2023-10-18 17:54:09 +0700

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,15 +3,11 @@ import { iconsData } from "./data";
 import React from "react";
 
 function Home() {
-  const icons = iconsData.map((icon) => <div id={icon.id}>{icon.icon}</div>);
+  const icons = iconsData.map((icon) => (
+    <img className="home-icon" src={icon.icon} alt="iocns" />
+  ));
 
-  return (
-    <div className="home">
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
+  return <div>{icons}</div>;
 }
 
 export default Home;
