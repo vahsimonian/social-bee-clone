65d65684571ed7ea602f93f957900631d4d9d2ce
vahsimonian <simonianvah@gmail.com>
2023-11-04 08:31:59 +0700

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -57,7 +57,7 @@ function Home() {
         <div className="icon-top">{icons}</div>
       </div>
       <div className={iconNavbarOpen}>
-        <div className="your-prompts">
+        <div className="your-prompts border-container">
           {/* <div></div> */}
           <div className="prompts-span-icon">
             <span>Your prompt</span>
