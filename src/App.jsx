107bbb1b4d18614251930c524ee22c0fffac6fa8
vahsimonian <simonianvah@gmail.com>
2023-11-16 20:49:58 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import SearchButton from "./SearchButton";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <SearchButton />
     </div>
   );
 }
